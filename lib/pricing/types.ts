5b302dd5761c0d4c256c6d9ec3aa6cc57fe9b83d
om3r305 <omer.30588@gmail.com>
2025-11-10 23:13:54 +0100

    chore: update pricing types definitions

--- lib/pricing/types.ts
+++ lib/pricing/types.ts
@@ -1,11 +1,19 @@
 // lib/pricing/types.ts
+export type FreebieInfo = {
+  allowed: number;
+  used: number;
+  discountedAmount: number;
+};
+
 export type PricingSummary = {
   merchandise: number;
   surcharges: number;
   subtotal: number;
   discount: number;
   total: number;
-  freebie?: { allowed: number; used: number; discountedAmount: number };
+  freebie?: FreebieInfo;
+
+  // aşağıdakiler opsiyonel – başka yerlerde kullanılıyor olabilir:
   meetsMin?: boolean;
   requiredMin?: number;
   plzKnown?: boolean;
