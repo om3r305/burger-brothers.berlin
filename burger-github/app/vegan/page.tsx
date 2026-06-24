07c98fa7e1fc576a6afcf38cb963c5ac18c5fe8e
om3r305 <omer.30588@gmail.com>
2026-06-24 21:59:32 +0200

    Remove accidental nested repository folder

--- burger-github/app/vegan/page.tsx
+++
@@ -1,7 +0,0 @@
-// app/vegan/page.tsx
-import { redirect } from "next/navigation";
-
-export default function VeganRedirect() {
-  // Eski /vegan linklerini tek sayfadaki vegan sekmesine taşıyoruz
-  redirect("/menu?cat=vegan");
-}
